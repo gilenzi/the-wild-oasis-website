495109426f5350d256517f76f8e0b0acb932c7ce
Ognjen Popovic <jasamtajogy@gmail.com>
2025-06-19 20:09:37 +0200

    in progress

--- app/layout.js
+++ app/layout.js
@@ -35,8 +35,8 @@ export default function RootLayout({children}) {
         className={`flex flex-col antialiased bg-primary-950 text-primary-100 relative min-h-screen ${josefin.className}`}
       >
         <Header />
-        <div className="flex-1 px-8 py-12">
-          <main className="max-w-7xl mx-auto">{children}</main>
+        <div className="flex-1 px-8 py-12 grid">
+          <main className="max-w-7xl mx-auto  w-full">{children}</main>
         </div>
       </body>
     </html>
